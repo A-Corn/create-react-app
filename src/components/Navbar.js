5203c5ae26fbe9c3e50d100b9df5995306e3f66d
Amir <89821062+A-Corn@users.noreply.github.com>
2023-02-21 16:55:56 +0000

    Fixed Navbar (no react router)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,28 +2,23 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import LinkContainer from 'react-router-bootstrap';
 
 function BasicExample() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <LinkContainer to="/">
-  <Navbar.Brand>CodeWithVincent</Navbar.Brand>
-  </LinkContainer>
+        <Navbar.Brand href="#home">CodeWithVincent</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <LinkContainer to="/about">
-    			<Nav.Link>About</Nav.Link>
-      		</LinkContainer>
+            <Nav.Link href="#link">About Us</Nav.Link>
             <NavDropdown title="Hackathon" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Info</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Rules
               </NavDropdown.Item>
-				<NavDropdown.Item href="#action/3.3">
+				<NavDropdown.Item href="#action/3.2">
                 FAQ
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Submit Project</NavDropdown.Item>
